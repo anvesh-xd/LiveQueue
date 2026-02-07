759d084bf906dcca6df3984c94db1b5a1012e199
anvesh-xd <asapkota2@crimson.ua.edu>
2026-02-07 14:58:30 -0600

    refactor : bootstrap errors fixed

--- frontend/components/BootstrapClient.tsx
+++ frontend/components/BootstrapClient.tsx
@@ -5,7 +5,7 @@ import { useEffect } from 'react';
 export default function BootstrapClient() {
   useEffect(() => {
     // Dynamically import Bootstrap JS only on the client side
-    import('bootstrap/dist/js/bootstrap.bundle.min.js');
+    import('bootstrap');
   }, []);
 
   return null;
